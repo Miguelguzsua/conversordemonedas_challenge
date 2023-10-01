861d683da94fcb69d177805ca18775294a81b73d
miguel <ismarokpunk@gmail.com>
2023-09-30 19:07:57 -0600

    esta es mi primer vercion de challengue 2

---
+++ src/conversorMonedas/ConvertirMonedasAPesos.java
@@ -0,0 +1,35 @@
+package conversorMonedas;
+
+import javax.swing.JOptionPane;
+
+public class ConvertirMonedasAPesos {
+	public void ConvertirDolaresAPesos(double valor) {
+		double monedaDolar = valor * 18.00;
+		monedaDolar = (double) Math.round(monedaDolar *100d)/100;
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaDolar+ " Pesos Mexicanos");
+	}
+	
+	public void ConvertirEurosAPesos(double valor) {
+		double monedaEuro = valor * 18.40;
+		monedaEuro = (double) Math.round(monedaEuro *100d)/100;
+		JOptionPane.showMessageDialog(null, "Tienes $ " +monedaEuro+ " Pesos Mexicanos");
+	}
+	
+	public void ConvertirLibrasAPesos(double valor) {
+		double monedaLibra = valor * 21.23;
+        monedaLibra = (double) Math.round(monedaLibra *100d)/100;
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaLibra+ " Pesos Mexicanos");
+	}
+	
+	public void ConvertirYenAPesos(double valor) {
+		double monedaYen = valor * 0.12;
+        monedaYen = (double) Math.round(monedaYen *100d)/100;
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaYen+ " Pesos Mexicanos");
+	}
+	
+	public void ConvertirWonAPesos(double valor) {
+		double monedaWon = valor * 0.01;
+        monedaWon = (double) Math.round(monedaWon *100d)/100;
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaWon+ " Pesos Mexicanos");
+	}
+}
